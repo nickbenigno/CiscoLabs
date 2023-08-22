ed40c1f10ef50b94d74d09a4c92a01e9d31da210
n.benigno12@gmail.com <n.benigno12@gmail.com>
2023-08-22 07:54:11 -0700

    Netmiko1 lab

---
+++ PythonScriptsENCORE/automationlabnetmiko1.py
@@ -0,0 +1,24 @@
+from netmiko import ConnectHandler
+
+cisco_l2 = {
+    'device_type': 'cisco_ios',
+    'host':   '192.168.8.132',
+    'username': 'user1',
+    'password': 'pass',
+    'port' : 22,          # optional, defaults to 22
+    'secret': 'pass',     # optional, defaults to ''
+}
+
+net_connect = ConnectHandler(**cisco_l2)
+output = net_connect.send_command('show ip int br')
+print (output)
+
+
+config_command = ['int loop 0', 'ip address 69.69.69.69 255.255.255.255']
+output = net_connect.send_config_set(config_command)
+print (output)
+
+for i in range (1,100):
+    config_command = ['vlan ' + str(i), 'name netmiko_VLAN ' + str(i)]
+    output = net_connect.send_config_set(config_command)
+    print (output)
